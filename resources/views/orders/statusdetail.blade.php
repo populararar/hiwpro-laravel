f5c23932aa28a67add7e7a9bb91ca08918e837fc
popular23p <popular23p@gmail.com>
2019-02-14 10:50:18 +0700

    10.50

--- resources/views/orders/statusdetail.blade.php
+++ resources/views/orders/statusdetail.blade.php
@@ -217,7 +217,7 @@
             <p class="font-gray">
               {{ $orderHeaders->seller_actual_price }} บาท </p>
           </p>
-          @endif
+        @endif
       </div>
 
 
@@ -434,8 +434,34 @@
         <p>ราคา {{ $sum }} บาท/ชิ้น</p>
       </div>
     </div>
-  </div>
-
+  </div> 
+  @if(!empty($orderHeaders->seller_actual_price))
+  <div class="page-header">
+      <h3>สินค้าที่จัดส่ง</h3>
+    </div>
+  <div class="shadow-sm  mb-5 bg-white rounded">
+      <div class="row">
+        <div class="col-md-5">
+            <img class='card-img-top w-50' src="{{ asset('/storage/'.$item->product->image_product_id) }}">
+        </div>
+       
+          <p>ราคาที่แม่ค้าได้แปะๆ : </p>
+          <p>
+            <p class="font-gray">
+              {{ $orderHeaders->seller_actual_price }} บาท </p>
+          </p>
+       
+        <div class="col-md-7 " style="padding-top:2%;">
+          <h1 style="border-left:5px solid #df3433; padding-left: 5px;">ชื่อสินค้า : {{$item->product->name}}</h1>
+          <p>ประเภทสินค้า : เครื่องสำอางค์</p>
+          <p>ชื่อผู้หิ้ว : {{$item->seller->name }}</p>
+          <p>จำนวน {{$item->qrt }} ชิ้น</p>
+         
+          <p>ราคา {{ $sum }} บาท/ชิ้น</p>
+        </div>
+      </div>
+    </div>
+  @endif
 
 
  @endforeach
